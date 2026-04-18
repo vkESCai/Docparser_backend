14218c5304fe1addfbcd71ee0869b96d4482607f
vkESCai <122730374+vkESCai@users.noreply.github.com>
2026-04-18 23:19:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000" ,"https://docparser-frontend.onrender.com"
+                  ],
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
